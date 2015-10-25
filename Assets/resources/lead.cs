94a5ec52973a54c5e40fa80f2a15de0348546c95
fersot100 <fersot100@yahoo.com>
2015-10-25 10:01:32 -0700

    Working Copy

--- Assets/resources/lead.cs
+++ Assets/resources/lead.cs
@@ -6,42 +6,48 @@ public class lead : MonoBehaviour {
     public GameObject leader;
     public Rigidbody2D player;
     public float speed, lerpspeed;
-    
+    public Camera main;
     private GameObject[] virus;
+    private float vnumber;
 
-    
 
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update() {
         //gets arrow movement
         key_movement();
         //instantiates an array of all virus objects
-        virus = GameObject.FindGameObjectsWithTag("Virus");
+        
 
     }
 
     public void FixedUpdate()
     {
+
+        virus = GameObject.FindGameObjectsWithTag("Virus");
         //for each virus move it towards the leader at lerpspeed
         foreach (GameObject obj in virus)
-            obj.transform.position = Vector2.MoveTowards(obj.transform.position, leader.transform.position,lerpspeed);
+            obj.transform.position = Vector2.MoveTowards(obj.transform.position,
+                leader.transform.position, lerpspeed);
     }
 
 
     void key_movement()
     {
 
-        if (Input.GetKey(KeyCode.RightArrow))
-            player.AddForce(transform.right * speed);
+            if (Input.GetKey(KeyCode.RightArrow))
+                player.AddForce(transform.right * speed);
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-            player.AddForce(-transform.right * speed);
+            if (Input.GetKey(KeyCode.LeftArrow))
+                player.AddForce(-transform.right * speed);
 
-        if (Input.GetKey(KeyCode.UpArrow))
-            player.AddForce(transform.up * speed);
+            if (Input.GetKey(KeyCode.UpArrow))
+                player.AddForce(transform.up * speed);
 
-        if (Input.GetKey(KeyCode.DownArrow))
-            player.AddForce(-transform.up * speed);
+            if (Input.GetKey(KeyCode.DownArrow))
+                player.AddForce(-transform.up * speed);
+        
     }
+ 
+
 }
--- Assets/resources/moveTest.cs
+++ Assets/resources/moveTest.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class moveTest : MonoBehaviour {
-    public GameObject virus;
+    public GameObject virus, healthyCells;
     private GameObject virusHold;
-    public Rigidbody2D virus_physics;
+    public Rigidbody2D virus_physics, healthyCells_physics;
     public float speed;
-    public int virusNumb;
     private Vector3 mousePosition, myForward, myRight;
+    private int cellHealth = 100;
+    
    
 
 
@@ -19,34 +21,33 @@ public class moveTest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-       // key_movement();
+        
         //virus_containermov();
-        mousePosition = Input.mousePosition;
-        //mouse_movement();
         stdMvmnt();
 
     }
-
-    void key_movement()
-    {
-
-        if (Input.GetKey(KeyCode.RightArrow))
-            virus_physics.AddForce(transform.right * speed);
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-            virus_physics.AddForce(-transform.right * speed);
-
-        if (Input.GetKey(KeyCode.UpArrow))
-            virus_physics.AddForce(transform.up * speed);
-
-        if (Input.GetKey(KeyCode.DownArrow))
-            virus_physics.AddForce(-transform.up * speed);
-    }
-
 
     void stdMvmnt()
     {
         virus_physics.AddTorque(Random.Range(-10.0F, 10.0F));
     }
+
+    void OnCollisionEnter2d(Collision2D col)
+
+    {
+        while (col.gameObject.tag == "Healthy Cell" && cellHealth > 0)
+            {
+            SpriteRenderer cellRenderer = GetComponent<SpriteRenderer>();
+            cellRenderer.color = new Color
+                (Mathf.SmoothStep(0, 0.5f,Time.time),
+                Mathf.SmoothStep(1, 0.5f, Time.time),
+                Mathf.SmoothStep(0, 0.5f, Time.time),
+                1);
+            cellHealth--;
+            }
+       
+    }    
+
+
 
 }
