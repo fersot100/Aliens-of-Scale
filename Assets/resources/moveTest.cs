13e2d9b5a916c9b06529f418cbbebaf5d3a02c92
magicdustin <magicdustin14@gmail.com>
2015-10-26 23:53:45 -0700

    kills viruses on colllision with t cells
    better watch out now!

--- Assets/resources/moveTest.cs
+++ Assets/resources/moveTest.cs
@@ -8,7 +8,7 @@ public class moveTest : MonoBehaviour {
     public Rigidbody2D virus_physics, healthyCells_physics;
     public float speed;
     private Vector3 mousePosition, myForward, myRight;
-    private int cellHealth = 100;
+ 
 
     // Use this for initialization
     void Start () {
@@ -29,19 +29,16 @@ public class moveTest : MonoBehaviour {
         virus_physics.AddTorque(Random.Range(-10.0F, 10.0F));
     }
 
-    void OnCollisionEnter2d(Collision2D col)
+    void OnCollisionEnter2D(Collision2D col)
 
     {
-        while (col.gameObject.tag == "Healthy Cell" && cellHealth > 0)
-            {
-            SpriteRenderer cellRenderer = GetComponent<SpriteRenderer>();
-            cellRenderer.color = new Color
-                (Mathf.SmoothStep(0, 0.5f,Time.time),
-                Mathf.SmoothStep(1, 0.5f, Time.time),
-                Mathf.SmoothStep(0, 0.5f, Time.time),
-                1);
-            cellHealth--;
-            }
+
+		if(col.gameObject.tag == "tcell")
+		{
+			Destroy(virus);
+			Debug.Log("FUCK IM DEAD");
+		}
+
        
     }    
 
